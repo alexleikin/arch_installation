4f670edbe8aa44a8d3fae8e1d43c3205edd58df5
alexleikin <alex.leeken@gmail.com>
2022-09-10 09:45:11 -0400

    fix

--- ar3.sh
+++ ar3.sh
@@ -6,11 +6,11 @@ makepkg --noconfirm -si
 
 sleep 5
 
-yay -S --noconfirm humanity-icon-theme otf-nerd-fonts-fira-mono otf-raleway ttf-mononoki ttf-ms-fonts nerd-fonts-fira-code 
+yay -S --noconfirm humanity-icon-theme otf-nerd-fonts-fira-mono otf-raleway ttf-mononoki ttf-ms-fonts nerd-fonts-fira-code picom-jonaburg-git
 
 sleep 5
 
-sudo pacman -S --noconfirm alacritty  nitrogen firefox pcmanfm rofi xorg xf86-video-amdgpu arandr wget ttf-font-awesome xorg-xinit  pulseaudio pulseaudio-alsa arc-gtk-theme arc-icon-theme cantarell-fonts cifs-utils copyq dunst flameshot gvfs hidapi imwheel lxappearance pasystray pavucontrol pcmanfm alsa-utils ttf-jetbrains-mono ttf-iosevka-nerd noto-fonts-emoji starship ttf-lato picom-jonaburg-git cronie
+sudo pacman -S --noconfirm alacritty  nitrogen firefox pcmanfm rofi xorg xf86-video-amdgpu arandr wget ttf-font-awesome xorg-xinit  pulseaudio pulseaudio-alsa arc-gtk-theme arc-icon-theme cantarell-fonts cifs-utils copyq dunst flameshot gvfs hidapi imwheel lxappearance pasystray pavucontrol pcmanfm alsa-utils ttf-jetbrains-mono ttf-iosevka-nerd noto-fonts-emoji starship ttf-lato cronie
 
 
 
